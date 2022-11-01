98a03a78b2d0fef8c0d703d0ecbbfff40f7d1eb7
MasterPasta001 <117233843+MasterPasta001@users.noreply.github.com>
2022-11-01 17:52:46 -0500

    Create RealSpammer.js

---
+++ RealSpammer.js
@@ -0,0 +1,45 @@
+let a;
+let n;
+let newname;
+const input = () => {
+    n = prompt("Name of Your Bots: ")
+    if(n == ''){
+        alert('Name Needs A Value!')
+        throw('Name Needs A Value!')
+    }
+  newname = prompt("Name you want to be after (cant be in the game yet): ")
+    if(newname == ''){
+        alert('Name Needs A Value!')
+        throw('Name Needs A Value!')
+    }
+    a = parseInt(prompt("How Many Bots Do You Want?: "));
+    if(a <= 0 || a == NaN){
+        alert('Amount Needs A Valid Number!')
+        throw('Amount Needs A Valid Number!')
+    }
+  
+}
+
+const entryPoint = Object.values(document.querySelector("#root"))[1]._context.provides.store
+client = {
+    hostId: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.hostId,
+    roomHash: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.roomHash,
+    localPlayer: JSON.parse(localStorage.previousContext).currentPlayer.playerId
+}
+
+const flood = (o, t, abc5) => {
+    for (i = 0; i < t; i++){
+
+        entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: `${o}-${i}`});
+        console.log(`${i}/${t} Bots Connected!`)
+      
+    }
+        entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: abc5});
+}
+
+input()
+flood(n, a, newname)
