eb0bea44bff034832237937487a242d1bacbd003
MasterPasta001 <117233843+MasterPasta001@users.noreply.github.com>
2022-11-02 17:01:05 -0500

    Update AntiKick.js

--- AntiKick.js
+++ AntiKick.js
@@ -11,10 +11,24 @@ client = {
     localPlayer: JSON.parse(localStorage.previousContext).currentPlayer.playerId
 }
 
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
 const flood = (o, t, abc5) => {
+    var oldname =  client.localPlayer;
     entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
         roomHash: client.roomHash,
         playerId: client.localPlayer});
+    entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: oldname});
 }
 
 input()
