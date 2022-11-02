46a5c051e3e870e275880b08e95115bdc11c4e05
MasterPasta001 <117233843+MasterPasta001@users.noreply.github.com>
2022-11-02 16:58:33 -0500

    Create AntiKick.js

---
+++ AntiKick.js
@@ -0,0 +1,21 @@
+let a;
+let n;
+let newname;
+const input = () => {
+}
+
+const entryPoint = Object.values(document.querySelector("#root"))[1]._context.provides.store
+client = {
+    hostId: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.hostId,
+    roomHash: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.roomHash,
+    localPlayer: JSON.parse(localStorage.previousContext).currentPlayer.playerId
+}
+
+const flood = (o, t, abc5) => {
+    entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: client.localPlayer});
+}
+
+input()
+flood(n, a, newname)
