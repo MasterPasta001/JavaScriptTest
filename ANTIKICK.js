2e3f91731e89584d0377fe65f7ebe55c1d90299f
MasterPasta001 <117233843+MasterPasta001@users.noreply.github.com>
2022-11-02 17:25:55 -0500

    Update ANTIKICK.js

--- ANTIKICK.js
+++ ANTIKICK.js
@@ -26,7 +26,7 @@ const flood = (o, t, abc5) => {
   var oldid = client.localPlayer
     entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
         roomHash: client.roomHash,
-        playerId: oldid + "_" + makeid(2)});
+        playerId: oldid + "ㅤ"});
 }
 
 input()
