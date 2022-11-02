4f00cbff164648312c4235e66ee4a7ec43a5da44
MasterPasta001 <117233843+MasterPasta001@users.noreply.github.com>
2022-11-02 17:02:46 -0500

    Create Realak.js

---
+++ Realak.js
@@ -0,0 +1,35 @@
+let a;
+let n;
+let newname;
+const input = () => {
+}
+
+const entryPoint = Object.values(document.querySelector("#root"))[1]._context.provides.store
+client = {
+    hostId: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.hostId,
+    roomHash: JSON.parse(JSON.stringify(entryPoint.getState())).game.data.roomHash,
+    localPlayer: JSON.parse(localStorage.previousContext).currentPlayer.playerId
+}
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+const flood = (o, t, abc5) => {
+    var oldname =  client.localPlayer;
+    entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: client.makeid(10)});
+    entryPoint._actions.connectSocket[0](), entryPoint._actions.joinRoom[0]({
+        roomHash: client.roomHash,
+        playerId: oldname});
+}
+
+input()
+flood(n, a, newname)
